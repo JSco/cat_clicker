942228e8eb44a0a5b5e295bb5ca84b4de0ead381
JSco <jackmichaelscotty@gmail.com>
2017-01-11 01:40:26 -0600

    added admin functionality and real cat pics

--- js/cat_clicker.js
+++ js/cat_clicker.js
@@ -1,47 +1,55 @@
 (function() {
-  var model = {
+
+  var model, octopus, catListView, catView, adminView;
+
+  model = {
+
+    showAdminView: false,
     selectedCat: null,
+
     cats: [{
-      name: "Josey",
+      name: "josey",
       clicks: 0,
-      image: "http://placehold.it/250x150"
+      image: "./images/cat-1.png"
     }, {
-      name: "Jimmy",
+      name: "jimmy",
       clicks: 0,
-      image: "http://placehold.it/250x160"
+      image: "./images/cat-2.png"
     }, {
-      name: "Jinxy",
+      name: "jinxy",
       clicks: 0,
-      image: "http://placehold.it/250x140"
+      image: "./images/cat-3.png"
     }, {
-      name: "Joey",
+      name: "joey",
       clicks: 0,
-      image: "http://placehold.it/250x170"
+      image: "./images/cat-4.png"
     }, {
-      name: "Kensington",
+      name: "kensington",
       clicks: 0,
-      image: "http://placehold.it/250x130"
+      image: "./images/cat-5.png"
     }]
+
   };
 
-  var octopus = {
+  octopus = {
 
     init: function() {
-      // model.init();
       catListView.init();
       catView.init();
-    },
-
-    getSelectedCat: function() {
-      return model.selectedCat;
+      adminView.init();
     },
 
     getAllCats: function() {
       return model.cats;
     },
 
+    getSelectedCat: function() {
+      return model.selectedCat;
+    },
+
     setSelectedCat: function(cat) {
       model.selectedCat = cat;
+      catView.render();
     },
 
     incrementCatClicks: function() {
@@ -49,14 +57,31 @@
       catView.render();
     },
 
+    setAdminViewProperty: function(show) {
+      model.showAdminView = show;
+      adminView.render();
+    },
+
+    getAdminViewProperty: function() {
+      return model.showAdminView;
+    },
+
+    saveAdminData: function(nameVal, clicksVal, imageVal) {
+      model.selectedCat.name = nameVal.toLowerCase();
+      model.selectedCat.clicks = parseInt(clicksVal, 10);
+      model.selectedCat.image = String(imageVal);
+      catView.render();
+      catListView.render();
+    }
+
   };
 
-  var catListView = {
+  catListView = {
+
     cat_list: null,
 
     init: function() {
       this.cat_list = document.getElementById('cat-list');
-
       this.render();
     },
 
@@ -76,7 +101,6 @@
         cat_list_item.addEventListener('click', (function(tempCat) {
           return function() {
             octopus.setSelectedCat(tempCat);
-            catView.render();
           };
         })(cats[cat]));
 
@@ -84,20 +108,28 @@
         this.cat_list.appendChild(cat_list_item);
       }
     }
+
   };
 
-  var catView = {
+  catView = {
+
     cat_name: null,
     cat_clicks: null,
     cat_image: null,
+    admin_button: null,
 
     init: function() {
       this.cat_name = document.getElementById('cat-name');
       this.cat_clicks = document.getElementById('cat-clicks');
       this.cat_image = document.getElementById('cat-image');
+      this.admin_button = document.getElementById('admin-button');
 
       this.cat_image.addEventListener('click', function() {
         octopus.incrementCatClicks();
+      });
+
+      this.admin_button.addEventListener('click', function() {
+        octopus.setAdminViewProperty(true);
       });
     },
 
@@ -107,6 +139,59 @@
       this.cat_name.innerHTML = selectedCat.name.charAt(0).toUpperCase() + selectedCat.name.substring(1);
       this.cat_clicks.innerHTML = selectedCat.clicks;
       this.cat_image.setAttribute('src', selectedCat.image);
+      this.admin_button.style.display = 'block';
+      adminView.render();
+    }
+
+  };
+
+  adminView = {
+
+    admin_button: null,
+    admin_form: null,
+    name_field: null,
+    clicks_field: null,
+    image_field: null,
+    cancel_button: null,
+    save_button: null,
+
+    init: function() {
+      this.admin_button = document.getElementById('admin-button');
+      this.admin_form = document.getElementById('admin-form');
+      this.name_field = document.getElementById('name-field');
+      this.clicks_field = document.getElementById('clicks-field');
+      this.image_field = document.getElementById('image-field');
+      this.cancel_button = document.getElementById('cancel-button');
+      this.save_button = document.getElementById('save-button');
+
+      this.cancel_button.addEventListener('click', function() {
+        octopus.setAdminViewProperty(false);
+      });
+
+      this.save_button.addEventListener('click', (function(nameField, clicksField, imageField) {
+        return function() {
+          octopus.saveAdminData(nameField.value, clicksField.value, imageField.value);
+        };
+      })(this.name_field, this.clicks_field, this.image_field));
+    },
+
+    render: function() {
+      var show = octopus.getAdminViewProperty(),
+        selectedCat = octopus.getSelectedCat();
+
+      if (show) {
+        this.admin_button.disabled = true;
+        this.admin_form.style.display = 'block';
+        this.name_field.value = selectedCat.name;
+        this.clicks_field.value = selectedCat.clicks;
+        this.image_field.value = selectedCat.image;
+      } else {
+        this.admin_button.disabled = false;
+        this.admin_form.style.display = 'none';
+        this.name_field.value = '';
+        this.clicks_field.value = '';
+        this.image_field.value = '';
+      }
     }
   };
 
